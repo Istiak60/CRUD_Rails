e1f2a7b5e1ff16f0aee79bc1d4bb4a4392d126af
Istiak60 <istisquare@gmail.com>
2022-06-30 01:22:19 +0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_103434) do
->>>>>>> rspec_init
+ActiveRecord::Schema[7.0].define(version: 2022_06_26_200458) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -27,15 +35,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_103434) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email", null: false
-    t.string "password"
-    t.string "phoneNo"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role_id"
-    t.index ["role_id"], name: "index_users_on_role_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "users", "roles"
+  add_foreign_key "comments", "articles"
 end
