fd3f5ec5be09cdc75ae80ea52d98fcd24a0476d6
Istiak60 <istisquare@gmail.com>
2022-06-30 12:04:36 +0600

    devise_bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-  
+  # Manual Authentication with Devise feature
+  # def index
+  #   if current_user.user_type == 'admin'
+  #   @users = User.order(created_at: :asc)
+  #   else
+  #     redirect_to root_path, alert: 'not_authorized'
+  #   end 
+  # end
+
   def index
-    @users = User.order(created_at: :asc) 
+    
+    @users = User.order(created_at: :asc)
+   
   end
+
   def show
     @users= User.find(params[:id])
   end
--- db/migrate/20220623103434_create_users.rb
+++ db/migrate/20220623103434_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :email, null: false, unique: true
       t.string :password
       t.string :phoneNo, unique: true
+      t.string :user_type
       t.timestamps
       t.references :role, foreign_key: true
     end
